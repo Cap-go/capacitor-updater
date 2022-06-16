b602d67efce3458fa77586917e14cf06232c0b3a
GitHub Action <action@github.com>
2022-06-16 01:21:16 +0000

    bump: version 3.3.8 → 3.3.9

--- android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdater.java
+++ android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdater.java
@@ -58,7 +58,7 @@ public class CapacitorUpdater {
 
     public String appId = "";
     public String deviceID = "";
-    public final String pluginVersion = "3.3.8";
+    public final String pluginVersion = "3.3.9";
     public String statsUrl = "";
 
     public CapacitorUpdater (final Context context) throws PackageManager.NameNotFoundException {
