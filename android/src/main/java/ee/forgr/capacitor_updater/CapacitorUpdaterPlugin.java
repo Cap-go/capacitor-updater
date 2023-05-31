962161d2299cee55b97abf5c3289d20ab5842bfb
Martin Donadieu <martindonadieu@gmail.com>
2023-05-31 17:13:38 +0100

    fix: revert version change

--- android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdaterPlugin.java
+++ android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdaterPlugin.java
@@ -55,7 +55,7 @@ public class CapacitorUpdaterPlugin
   private static final String channelUrlDefault =
     "https://api.capgo.app/channel_self";
 
-  private final String PLUGIN_VERSION = "6.0.0";
+  private final String PLUGIN_VERSION = "5.0.0";
   private static final String DELAY_CONDITION_PREFERENCES = "";
 
   private SharedPreferences.Editor editor;
