14373a4679668b0da5c4722e7ee7006d64ae743d
WcaleNieWolny <isupermichael007@gmail.com>
2025-01-24 06:09:01 +0100

    feat: capacitor 7

--- CapgoCapacitorUpdater.podspec
+++ CapgoCapacitorUpdater.podspec
@@ -11,7 +11,7 @@ Pod::Spec.new do |s|
   s.author = package['author']
   s.source = { :git => package['repository']['url'], :tag => s.version.to_s }
   s.source_files = 'ios/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
-  s.ios.deployment_target  = '13.0'
+  s.ios.deployment_target = '14.0'
   s.dependency 'Capacitor'
   s.dependency 'SSZipArchive', '2.4.3'
   s.dependency 'Alamofire'
--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapgoCapacitorUpdater",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapgoCapacitorUpdater",
             targets: ["CapacitorUpdaterPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.4.3"),
         .package(url: "https://github.com/mrackwitz/Version.git", .upToNextMajor(from: "0.8.0"))
