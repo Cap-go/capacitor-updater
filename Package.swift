b804097dedd3c5949d8fd1344ee029ea46e134ad
Martin Donadieu <martindonadieu@gmail.com>
2025-02-17 15:09:11 +0000

    fix: syntaxt SPM

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.1"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .upToNextMajor(exact: "2.4.3")),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.4.3"),
         .package(url: "https://github.com/mxcl/Version.git", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
