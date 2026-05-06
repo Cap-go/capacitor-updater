1d67a90ea59995a6c98d16b143c7d44d519b1889
Martin Donadieu <martindonadieu@gmail.com>
2026-05-06 11:56:20 +0200

    chore: restore example app swiftpm range

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/.bun/@capgo+capacitor-updater@file+../node_modules/@capgo/capacitor-updater")
     ],
     targets: [
